e57c1111b8638595884b5455b269ad17a9204473
Marcel Stimberg <marcel.stimberg@inserm.fr>
2022-07-13 17:17:37 +0200

    write result files to MEMFS

--- brian2wasm/device.py
+++ brian2wasm/device.py
@@ -19,12 +19,17 @@ class WASMStandaloneDevice(CPPStandaloneDevice):
     """
     The `Device` used for WASM simulations.
     """
+    def __init__(self, *args, **kwds):
+        super(WASMStandaloneDevice, self).__init__(*args, **kwds)
+        # Overwrite the templater to prefer our templates
+        self.code_object_class().templater = self.code_object_class().templater.derive('brian2wasm')
+
+
     def generate_makefile(self, writer, compiler, compiler_flags, linker_flags, nb_threads, debug):
         compiler_flags = '-Ibrianlib/randomkit'
         linker_flags = '-Lbrianlib/randomkit'
         preloads = ' '.join(f'--preload-file static_arrays/{static_array}'
                             for static_array in sorted(self.static_arrays.keys()))
-        templater = self.code_object_class().templater.derive('brian2wasm')
         rm_cmd = 'rm $(OBJS) $(PROGRAM) $(DEPS)'
         if debug:
             compiler_debug_flags = '-g -DDEBUG'
@@ -37,7 +42,7 @@ class WASMStandaloneDevice(CPPStandaloneDevice):
         source_files = source_files.replace('brianlib/randomkit/randomkit.c', 
                                             'brianlib/randomkit/randomkit.cpp')
         
-        makefile_tmp = templater.makefile(None, None,
+        makefile_tmp = self.code_object_class().templater.makefile(None, None,
             source_files=source_files,
             header_files=' '.join(sorted(writer.header_files)),
             compiler_flags=compiler_flags,
@@ -48,6 +53,7 @@ class WASMStandaloneDevice(CPPStandaloneDevice):
             rm_cmd=rm_cmd)
         writer.write('makefile', makefile_tmp)
    
+
     def copy_source_files(self, writer, directory):
         super(WASMStandaloneDevice, self).copy_source_files(writer, directory)
         # Rename randomkit.c so that emcc compiles it to wasm
