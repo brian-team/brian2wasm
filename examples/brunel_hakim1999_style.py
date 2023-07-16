6037051d01d2a20b0ce368cf1173e533a67c7941
Marcel Stimberg <marcel.stimberg@inserm.fr>
2023-07-16 14:57:06 +0200

    Add example for styled page with chart.js

---
+++ examples/brunel_hakim1999_style.py
@@ -0,0 +1,29 @@
+from brian2 import *
+import brian2wasm
+set_device('wasm_standalone', directory='brunel_hakim1999_style')
+N = 5000
+Vr = 10*mV
+theta = 20*mV
+tau = 20*ms
+delta = 2*ms
+taurefr = 2*ms
+duration = .1*second
+C = 1000
+sparseness = float(C)/N
+J = .1*mV
+muext = 25*mV
+sigmaext = 1*mV
+
+eqs = """
+dV/dt = (-V+muext + sigmaext * sqrt(tau) * xi)/tau : volt
+"""
+
+group = NeuronGroup(N, eqs, threshold='V>theta',
+                    reset='V=Vr', refractory=taurefr, method='euler')
+group.V = Vr
+conn = Synapses(group, group, on_pre='V += -J', delay=delta)
+conn.connect(p=sparseness)
+M = SpikeMonitor(group)
+LFP = PopulationRateMonitor(group)
+
+run(duration, report='text', report_period=0.1*second)
