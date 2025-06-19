a0e9014151913678c999a9b625467da54179fe8e
Palash Chitnavis <palashchitnavis@gmail.com>
2025-06-19 09:40:44 +0530

    github workflow

--- brian2wasm/emsdk_setup.sh
+++ brian2wasm/emsdk_setup.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 # emsdk_setup.sh
 
-# Paths to emsdk and emsdk_env.sh in the Pixi environment
-EMSDK_EXEC=".pixi/envs/default/bin/emsdk"
-EMSDK_ENV=".pixi/envs/default/lib/python${PYTHON_VERSION:-3.13}/site-packages/emsdk/emsdk_env.sh"
-
-echo "Checking emsdk executable..."
-# Check if emsdk executable exists
-if [ ! -f "$EMSDK_EXEC" ]; then
-  echo "Error: emsdk executable not found at $EMSDK_EXEC."
-  exit 1
-fi
+# Path to emsdk_env.sh in the Pixi environment
+EMSDK_ENV="${CONDA_EMSDK_DIR}/emsdk_env.sh"
 
 echo "Checking emsdk_env.sh..."
 # Check if emsdk_env.sh exists
@@ -18,11 +10,6 @@ if [ ! -f "$EMSDK_ENV" ]; then
   echo "Error: emsdk_env.sh not found at $EMSDK_ENV."
   exit 1
 fi
-
-echo "Installing and activating latest Emscripten SDK..."
-# Install and activate the latest Emscripten SDK
-"$EMSDK_EXEC" install latest
-"$EMSDK_EXEC" activate latest
 
 echo "Sourcing Emscripten environment..."
 # Source the environment script to set up PATH and other variables
