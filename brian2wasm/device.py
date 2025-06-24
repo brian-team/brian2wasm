f61186262d827715a10b5d0e84cac6cc569510ee
Palash Chitnavis <palashchitnavis@gmail.com>
2025-06-24 12:43:59 +0530

    add example to github action

--- brian2wasm/device.py
+++ brian2wasm/device.py
@@ -341,6 +341,10 @@ class WASMStandaloneDevice(CPPStandaloneDevice):
                 shutil.copy(html_file, os.path.join(self.project_dir, 'index.html'))
 
         with in_directory(directory):
+            if os.environ.get('BRIAN2WASM_NO_SERVER','0') == '1':
+                print("Skipping server startup (--no-server flag set)")
+                return
+
             if prefs.devices.wasm_standalone.emsdk_directory:
                 emsdk_path = prefs.devices.wasm_standalone.emsdk_directory
                 run_cmd = ['source', f'{emsdk_path}/emsdk_env.sh', '&&', 'emrun', 'index.html']
